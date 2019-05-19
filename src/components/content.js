5571792bb41cbb9eb90985d79995bb4ec2ae6eb4
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2019-05-19 13:25:25 +0530

    Initial commit

---
+++ src/components/content.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class content extends Component {
+  render() {
+    return (
+      <div>
+        This is content
+      </div>
+    )
+  }
+}
