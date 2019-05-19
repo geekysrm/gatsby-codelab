1b8a403fdfca1e44d016d8a43da564c8c44232fa
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2019-05-19 13:45:46 +0530

    Add gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,18 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path')
+const fs = require('fs')
 
-// You can delete this file if you're not using it
+exports.createPages = async({actions}) => {
+    const data = JSON.parse(
+        fs.readFileSync('src/data/programs.json')
+    )
+    const blogPostTemplate = path.resolve('src/templates/template.js')
+
+    data.pages.forEach((item) => {
+        console.log(item)
+        createPage({
+            path: item.slug,
+            component:blogPostTemplate,
+            context: item
+        })
+    })
+}
